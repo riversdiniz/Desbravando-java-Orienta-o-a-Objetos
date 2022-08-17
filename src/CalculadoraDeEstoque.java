d3704bf5fa9b8a1772c78cb97f5374efcdcdb5d1
River Diniz <river-s-diniz@hotmail.com>
2022-08-17 17:04:54 -0300

    Criando a primeira classe da atividade do livro.

---
+++ src/CalculadoraDeEstoque.java
@@ -0,0 +1,10 @@
+public class CalculadoraDeEstoque {
+    public static void main(String[] args) {
+        double livroJava = 59.90;
+        double livroTDD = 59.90;
+
+        double soma = livroJava + livroTDD;
+
+        System.out.println("O total em estoque é " + soma);
+    }
+}
---
+++ src/MeuPrimeiroPrograma.java
@@ -0,0 +1,5 @@
+public class MeuPrimeiroPrograma {
+    public static void main(String[] args) {
+        System.out.println("O primeiro de muitos!");
+    }
+}
