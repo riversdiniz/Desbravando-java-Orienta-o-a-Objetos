768e28d0af26e63b5d0cba7bf89f102cb638d987
River Diniz <river-s-diniz@hotmail.com>
2022-08-24 17:01:00 -0300

    Removendo a linha d método antigo.

--- src/br/com/livraria/CadastroDeLivros.java
+++ src/br/com/livraria/CadastroDeLivros.java
@@ -10,10 +10,7 @@ public class CadastroDeLivros {
         livro.valor = 59.90;
         livro.isbn = "978-85-66250-46-6";
 
-        System.out.println(livro.nome);
-        System.out.println(livro.descricao);
-        System.out.println(livro.valor);
-        System.out.println(livro.isbn);
+        livro.mostrarDetalhes();
 
         Livro outroLivro = new Livro();
         outroLivro.nome = "Lógica de Programação";
@@ -21,10 +18,6 @@ public class CadastroDeLivros {
         outroLivro.valor = 59.90;
         livro.isbn = "978-85-66250-22-0";
 
-        System.out.println(outroLivro.nome);
-        System.out.println(outroLivro.descricao);
-        System.out.println(outroLivro.valor);
-        System.out.println(outroLivro.isbn);
-
+        livro.mostrarDetalhes();
     }
 }
