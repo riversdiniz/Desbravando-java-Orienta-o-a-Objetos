cdffd602e759fb86b34127efd1303c3e098bf548
River Diniz <river-s-diniz@hotmail.com>
2022-08-24 16:14:49 -0300

    Looping com while.

--- src/CalculadoraDeEstoque.java
+++ src/CalculadoraDeEstoque.java
@@ -2,16 +2,19 @@ public class CalculadoraDeEstoque {
     public static void main(String[] args) {
 
         double soma = 0;
+        int contador = 0;
 
-        for (double i = 0; i < 35 ; i ++){
-            soma += 59.90;
+        while (contador < 35) {
+            double valorDoLivro = 59.90;
+            soma = soma + valorDoLivro;
+            contador = contador + 1;
         }
 
         System.out.println("O total em estoque é " + soma);
 
         if (soma < 150) {
             System.out.println("Seu estoque está muito baixo!");
-        } else if(soma >= 2000) {
+        } else if (soma >= 2000) {
             System.out.println("Seu estoque está muito alto");
         } else {
             System.out.println("Seu estoque está bom");
