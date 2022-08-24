ddcc773966c443d8fb92ce6d9f6e4e75ac587649
River Diniz <river-s-diniz@hotmail.com>
2022-08-24 17:04:43 -0300

    Alterando o método do código.

--- src/br/com/livraria/Livro.java
+++ src/br/com/livraria/Livro.java
@@ -7,11 +7,11 @@ public class Livro {
     String isbn;
 
     void mostrarDetalhes() {
-        String mensagem = "Mostrando detalhes do livro ";
-        System.out.println(mensagem);
-        System.out.println(nome);
-        System.out.println(descricao);
-        System.out.println(valor);
-        System.out.println(isbn);
+        System.out.println("Mostrando detablhes do livro");
+        System.out.println("Nome " + nome);
+        System.out.println("Descrição " + descricao);
+        System.out.println("Valor " + valor);
+        System.out.println("ISBN: " + isbn);
+        System.out.println("--");
     }
 }
