07978299cdef4d41a30dd4c0dcca849515de6980
River Diniz <river-s-diniz@hotmail.com>
2022-08-24 17:30:55 -0300

    Criando método mostrar

--- src/br/com/livraria/Autor.java
+++ src/br/com/livraria/Autor.java
@@ -5,4 +5,10 @@ public class Autor {
     String email;
     String cpf;
 
+    void mostrarDetalhes() {
+        System.out.println("Mostrando detalhes do autor ");
+        System.out.println("Nome: " + nome);
+        System.out.println("Email: " + email);
+        System.out.println("CPF: " + cpf);
+    }
 }
