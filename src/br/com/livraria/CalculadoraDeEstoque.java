ba407d17df70653c5eda8255a08770e9e5ecb7fa
River Diniz <river-s-diniz@hotmail.com>
2022-08-24 16:36:56 -0300

    Criando um pacote para nosso sistema.

--- src/br/com/livraria/CalculadoraDeEstoque.java
+++ src/br/com/livraria/CalculadoraDeEstoque.java
@@ -1,13 +1,12 @@
+package br.com.livraria;
+
 public class CalculadoraDeEstoque {
     public static void main(String[] args) {
 
         double soma = 0;
-        int contador = 0;
 
-        while (contador < 35) {
-            double valorDoLivro = 59.90;
-            soma += valorDoLivro;
-            contador += 1;
+        for (double i = 0; i < 35; i++) {
+            soma += 59.90;
         }
 
         System.out.println("O total em estoque é " + soma);
