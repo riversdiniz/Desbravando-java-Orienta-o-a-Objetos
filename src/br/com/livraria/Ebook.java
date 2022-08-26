de1846b94fb3835638f4d1e4b6304e795836d7c2
River Diniz <river-s-diniz@hotmail.com>
2022-08-26 15:57:49 -0300

    Criando uma nova classe Ebook

---
+++ src/br/com/livraria/Ebook.java
@@ -0,0 +1,14 @@
+package br.com.livraria;
+
+public class Ebook extends Livro {
+    
+    private String waterMark;
+
+    public void setWaterMark() {
+        this.waterMark = waterMark;
+    }
+
+    public String getWaterMark() {
+        return waterMark;
+    }
+}
--- src/br/com/livraria/Livro.java
+++ src/br/com/livraria/Livro.java
@@ -6,13 +6,12 @@ public class Livro {
     private double valor;
     private String isbn;
     private Autor autor;
+    private boolean impresso;
 
     public Livro(Autor autor){
         this.autor = autor;
-    }
-
-    public Livro(){
-        
+        this.isbn = "000-00-00000-00-0";
+        this.impresso = true;
     }
 
     public String getNome() {
@@ -67,6 +66,8 @@ public class Livro {
 
     public boolean aplicaDescontoDe(double porcentagem) {
         if (porcentagem > 0.3) {
+            return false;
+        } else if(!this.impresso && porcentagem > 0.15){
             return false;
         }
         this.valor -= this.valor * porcentagem;
