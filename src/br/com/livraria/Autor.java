8acb31948782051809375ad009591ddc96585d8f
River Diniz <river-s-diniz@hotmail.com>
2022-08-26 15:18:00 -0300

    Criando getters e setters do autor.

--- src/br/com/livraria/Autor.java
+++ src/br/com/livraria/Autor.java
@@ -5,6 +5,30 @@ public class Autor {
     String email;
     String cpf;
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
     void mostrarDetalhes() {
         System.out.println("Mostrando detalhes do autor ");
         System.out.println("Nome: " + nome);
