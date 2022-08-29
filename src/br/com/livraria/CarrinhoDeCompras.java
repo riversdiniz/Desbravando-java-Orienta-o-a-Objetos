5e05760f0fb0417ab21450fd258d437168b2c560
River Diniz <river-s-diniz@hotmail.com>
2022-08-29 17:30:33 -0300

    Adicionando uma superclasse linha 5.

--- src/br/com/livraria/CarrinhoDeCompras.java
+++ src/br/com/livraria/CarrinhoDeCompras.java
@@ -2,11 +2,7 @@ package br.com.livraria;
 
 public class CarrinhoDeCompras {
     
-    public void adiciona(LivroFisico livro) {
-        System.out.println("Adicionando: " + livro);
-    }
-
-    public void adiciona(Ebook livro) {
+    public void adiciona(Livro livro) {
         System.out.println("Adicionando: " + livro);
     }
 }
