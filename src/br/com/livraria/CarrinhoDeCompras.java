5f80a14e191f08bfb6da677d7ca9c7f0bca8dca6
River Diniz <river-s-diniz@hotmail.com>
2022-08-29 17:28:36 -0300

    Criando uma nova classe CarrinhoDeCompras

---
+++ src/br/com/livraria/CarrinhoDeCompras.java
@@ -0,0 +1,12 @@
+package br.com.livraria;
+
+public class CarrinhoDeCompras {
+    
+    public void adiciona(LivroFisico livro) {
+        System.out.println("Adicionando: " + livro);
+    }
+
+    public void adiciona(Ebook livro) {
+        System.out.println("Adicionando: " + livro);
+    }
+}
