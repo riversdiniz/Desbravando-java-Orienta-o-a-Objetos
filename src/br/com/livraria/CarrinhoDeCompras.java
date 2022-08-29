ee14c0e0d67707da1449b20a736df19852a4075a
River Diniz <river-s-diniz@hotmail.com>
2022-08-29 17:44:45 -0300

    Add atritubo.

--- src/br/com/livraria/CarrinhoDeCompras.java
+++ src/br/com/livraria/CarrinhoDeCompras.java
@@ -1,8 +1,16 @@
 package br.com.livraria;
 
 public class CarrinhoDeCompras {
+
+    private double total;
     
     public void adiciona(Livro livro) {
         System.out.println("Adicionando: " + livro);
+        livro.aplicaDescontoDe(0.05);
+        total += livro.getValor();
+    }
+
+    public double getTotal() {
+        return total;
     }
 }
