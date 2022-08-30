2c2adbb1b35680970337eeecc8492f935f5b9ace
River Diniz <river-s-diniz@hotmail.com>
2022-08-30 17:59:33 -0300

    Criando uma nova classe date

--- src/br/com/livraria/Promocional.java
+++ src/br/com/livraria/Promocional.java
@@ -1,6 +1,14 @@
 package br.com.livraria;
 
+import java.beans.AppletInitializer;
+
+@FunctionalInterface
 public interface Promocional {
     
     boolean aplicaDescontoDe(double porcentagem);
+    // boolean naoSouMaisUmaInterfaceFuncional();
+
+    default boolean aplicaDescontoDe10Porcento() {
+        return aplicaDescontoDe(0.1);
+    }
 }
