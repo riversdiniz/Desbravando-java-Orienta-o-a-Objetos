12b64eb36db4dc2c35b2b1827c314490b09ecec7
River Diniz <river-s-diniz@hotmail.com>
2022-08-31 17:51:23 -0300

    Desabilitado comentario

--- src/br/com/casadocodigo/produtos/CarrinhoDeCompras.java
+++ src/br/com/casadocodigo/produtos/CarrinhoDeCompras.java
@@ -5,7 +5,7 @@ public class CarrinhoDeCompras {
     private double total;
     private Produto[] produtos = new Produto[10];
     private int contador = 0;
-
+    
     public void adiciona(Produto produto) {
         System.out.println("Adicionando: " + produto);
         this.produtos[contador] = produto;
