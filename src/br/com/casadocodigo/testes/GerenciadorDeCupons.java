3713fb3afc4feee0ba6245665c5593a27fce0cf6
River Diniz <river-s-diniz@hotmail.com>
2022-09-05 13:49:19 -0300

    Criando classe gerenciador de cupons

---
+++ src/br/com/casadocodigo/testes/GerenciadorDeCupons.java
@@ -0,0 +1,14 @@
+package br.com.casadocodigo.testes;
+
+import java.util.Arrays;
+
+public class GerenciadorDeCupons {
+    
+    private List<String> cupons;
+
+    public GerenciadorDeCupons() {
+        this.cupons = Arrays.asList(" CUP 74 ", " CUP 158 ", 
+        " CUP 14 ", " CUP 52 ", " CUP 21 ", " CUP 221 ", " CUP 91 ", 
+        " CUP 327 ", " CUP 410 ", " CUP 275 ", " CUP 484 ", " CUP 207 ");
+    }
+}
