8b1c4aa898bf48c74b99f3822602572b6154a8ad
River Diniz <river-s-diniz@hotmail.com>
2022-09-05 14:08:22 -0300

    Fazendo a mudança para hashset

--- src/br/com/casadocodigo/testes/GerenciadorDeCupons.java
+++ src/br/com/casadocodigo/testes/GerenciadorDeCupons.java
@@ -1,15 +1,20 @@
 package br.com.casadocodigo.testes;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class GerenciadorDeCupons {
     
-    private List<String> cupons;
+    private Set<String> cupons;
 
     public GerenciadorDeCupons() {
-        this.cupons = Arrays.asList(" CUP 74 ", " CUP 158 ", 
+        this.cupons = new HashSet<String>();
+
+        cupons.addAll(Arrays.asList(" CUP 74 ", " CUP 158 ", 
         " CUP 14 ", " CUP 52 ", " CUP 21 ", " CUP 221 ", " CUP 91 ", 
-        " CUP 327 ", " CUP 410 ", " CUP 275 ", " CUP 484 ", " CUP 207 ");
+        " CUP 327 ", " CUP 410 ", " CUP 275 ", " CUP 484 ", " CUP 207 "));
     }
 
     public boolean validaCupom(String cupom) {
